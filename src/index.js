641c3b0f5ad2b306a4b354015673c80a7bf648ed
Fede <efesjotaseles2@gmail.com>
2022-11-07 15:06:56 -0300

    Intento de hola mundo

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { ReactDOM } from 'react';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const holaMundo = <p>Hola mundo!</p>
+root.render(holaMundo);
